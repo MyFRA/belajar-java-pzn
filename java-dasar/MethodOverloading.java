0fe2191ab7a2aa00d77ea149ed440b40c99dc5a1
MyFRA <tomyntapss@gmail.com>
2026-01-29 17:00:37 +0700

    java-dasar: MethodOverloading

---
+++ java-dasar/MethodOverloading.java
@@ -0,0 +1,16 @@
+public class MethodOverloading {
+
+    public static void main(String[] args) {
+        sayHello("Tomy");
+        sayHello("Tomy", "Wibowo");
+    }
+
+    static void sayHello(String firstName) {
+        System.out.println("Hello " + firstName);
+    }
+
+    static void sayHello(String firstName, String lastName) {
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+
+}
