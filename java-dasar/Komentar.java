c2f56f5406f0f43ada2da4e9e1e9ca952a080c4e
MyFRA <tomyntapss@gmail.com>
2026-01-29 17:11:12 +0700

    java-dasar: Komentar

---
+++ java-dasar/Komentar.java
@@ -0,0 +1,18 @@
+public class Komentar {
+
+    public static void main(String[] args) {
+        System.out.println(sum(50, 20));
+    }
+
+    /**
+     * 
+     * @param a nilai a
+     * @param b nilai b
+     * @return a + b
+     */
+    static int sum(int a, int b) {
+        // Jumlahkan a + b
+        return a + b;
+    }
+
+}
