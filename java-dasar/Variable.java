7f0b4978ec6d0ec5de22e97f5d007be0e454c1f8
MyFRA <tomyntapss@gmail.com>
2026-01-29 15:21:09 +0700

    java-dasar: Variable

---
+++ java-dasar/Variable.java
@@ -0,0 +1,27 @@
+public class Variable {
+
+    public static void main(String[] args) {
+        
+        String name = "Tomy Wibowo";
+        int age = 23;
+        String address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+
+        // var Sintaks
+        var name2 = "Fra";
+        int age2 = 22;
+        String address2 = "Indonesia";
+
+        System.out.println(name2);
+        System.out.println(age2);
+        System.out.println(address2);
+
+        // Kata Kunci Final (Konstanta)
+        final float phi = 3.14F;
+        System.out.println(phi);
+        
+    }
+}
