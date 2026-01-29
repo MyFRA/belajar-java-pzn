987561ed1de441c6c75f26b46ab595630482371b
MyFRA <tomyntapss@gmail.com>
2026-01-29 15:07:17 +0700

    java-dasar: TipeDataChar

---
+++ java-dasar/TipeDataChar.java
@@ -0,0 +1,14 @@
+public class TipeDataChar {
+
+    public static void main(String[] args) {
+        
+        char f = 'F';
+        char r = 'R';
+        char a = 'A';
+
+        System.out.println(f);
+        System.out.println(r);
+        System.out.println(a);
+    }
+
+}
