14a52c257749d8d8419094cc1646f8f74af317a5
MyFRA <tomyntapss@gmail.com>
2026-01-29 16:50:52 +0700

    java-dasar: MethodReturnValue

---
+++ java-dasar/MethodReturnValue.java
@@ -0,0 +1,12 @@
+public class MethodReturnValue {
+
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2));
+        System.out.println(sum(10, 12));
+    }
+
+    static int sum(int value1, int value2) {
+        return  value1 + value2;
+    }
+
+}
