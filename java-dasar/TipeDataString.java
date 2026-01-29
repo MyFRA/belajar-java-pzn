ed2ad9806c1dc869262cab3de226bc57f091f615
MyFRA <tomyntapss@gmail.com>
2026-01-29 15:12:15 +0700

    java-dasar: TipeDataString

---
+++ java-dasar/TipeDataString.java
@@ -0,0 +1,12 @@
+public class TipeDataString {
+
+    public static void main(String[] args) {
+        
+        String firstName = "Tomy";
+        String lastName = "Wibowo";
+
+        System.out.println(firstName);
+        System.out.println(lastName);        
+    }
+
+}
