f4ded00633f4eec08627592031c431985d11254f
MyFRA <tomyntapss@gmail.com>
2026-01-30 15:28:07 +0700

    java-oop: object

---
+++ java-oop/src/ObjectJava.java
@@ -0,0 +1,13 @@
+public class ObjectJava {
+
+    public static void main(String[] args) {
+        
+        var person1 = new ClassJava();
+        ClassJava person2 = new ClassJava();
+
+        System.out.println(person1);
+        System.out.println(person2);
+
+    }
+
+}
