4ab44db5302af8de7d1aff519aa7444034eb8dcc
MyFRA <tomyntapss@gmail.com>
2026-01-30 19:02:08 +0700

    java-oop: access level

---
+++ javaoop/src/accesslevel/App.java
@@ -0,0 +1,9 @@
+package accesslevel;
+
+public class App {
+    //  ada public, protected, no modifier dan private
+    //  public → bebas diakses dari mana saja
+    //  protected → satu package + subclass
+    //  default → hanya satu package
+    //  private → hanya dalam satu class
+}
