a40bdbe9e46b68d073e13b49bcd4ced3125fb134
MyFRA <tomyntapss@gmail.com>
2026-02-02 21:17:42 +0700

    java-oop: abstract method

---
+++ javaoop/src/abstractmethod/App.java
@@ -0,0 +1,10 @@
+package abstractmethod;
+
+public class App {
+
+    //  Saat kita membuat class yang abstract, kita bisa membuat abstract method juga di dalam class abstract tsb
+    //  Saat kita membuat sebuah abstract method, kita tidak boleh membuat block method untuk method tsb
+    //  Artinya, abstract method wajib di override di class child
+    //  Abstrack method tidak boleh memiliki access modifier private
+
+}
