1143c039aa95972127643a1d81be2d1c4b635c23
MyFRA <tomyntapss@gmail.com>
2026-02-04 11:18:53 +0700

    java-oop: encapsulation getter & setter

---
+++ javaoop/src/encapsulationsettergetter/Category.java
@@ -0,0 +1,16 @@
+package encapsulationsettergetter;
+
+public class Category {
+
+    private String id;
+    private boolean isExpensive;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
