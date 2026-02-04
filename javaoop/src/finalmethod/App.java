4bd2ad7fd0be62e20e0c1c0c70b3278dec5cb4b0
MyFRA <tomyntapss@gmail.com>
2026-02-04 14:12:51 +0700

    java-oop: final method

---
+++ javaoop/src/finalmethod/App.java
@@ -0,0 +1,12 @@
+package finalmethod;
+
+public class App {
+
+    static void main() {
+
+        SocialMedia socialMedia = new SocialMedia();
+        System.out.println(socialMedia.getAmountLikes());;
+
+    }
+
+}
---
+++ javaoop/src/finalmethod/SocialMedia.java
@@ -0,0 +1,10 @@
+package finalmethod;
+
+final class SocialMedia {
+
+    // Final method artinya method tersebut tidak boleh di override di child class nya
+    final int getAmountLikes() {
+        return 10;
+    }
+
+}
