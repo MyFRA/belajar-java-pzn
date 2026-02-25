bc50c907ed1f2a99dea58bcf747b70e4d65445a3
MyFRA <tomyntapss@gmail.com>
2026-02-25 23:48:41 +0700

    java-standard: ScannerClass

---
+++ standard-classes/src/scannerclass/App.java
@@ -0,0 +1,41 @@
+package scannerclass;
+
+import java.util.Scanner;
+
+public class App {
+
+    static void main() {
+
+        /**
+         * Scanner Class
+         *
+         * Scanner sebenarnya bagian dari Java IO(Input Output), dan ini akan dibahas di materi terpisah
+         * Namun sekarang kita akan bahas sekilas tentang class Scanner
+         * Class Scanner hadir sejak Java 5
+         * Class Scanner adalah class yang bisa digunakan untuk membaca input, entah dari file, console, dan lain lain
+         * Class Scanner ini cocok untuk dijadikan object untuk membaca input user saat kita belajar membuat program Java menggunakan console/terminal
+         *
+         */
+
+        /**
+         * Method di Scanner Class
+         *
+         * Method Keterangan
+         * nextLine() -> Membaca string
+         * nextInt() -> Membaca int
+         * nextLong() -> Membaca long
+         * nextBoolean() -> Membaca boolean
+         * ...dan masih banyak lagi
+         *
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nama: ");
+        String nama = scanner.nextLine();
+
+        System.out.println("Hello " + nama);
+
+    }
+
+}
